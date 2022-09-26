d75cfbbf4021888d5646d6a6d47233938b97c19d
KudakwasheM <masayakudakwashe@gmail.com>
2022-09-26 12:10:31 +0200

    Home Controller

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [AuthController::class, 'loginPage']);
+Route::get('/', function () {
+    return view('home');
+});
 
 Route::get('/login', [AuthController::class, 'loginPage']);
 Route::post('/login-post', [AuthController::class, 'login'])->name('login');
